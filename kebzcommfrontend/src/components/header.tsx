cd25dd7e63a1e1f5ca5c9c1dd7dfead3d82d23c7
EW <96669967+bigbrrrr@users.noreply.github.com>
2024-04-08 15:42:25 -0500

    header styling

--- kebzcommfrontend/src/components/header.tsx
+++ kebzcommfrontend/src/components/header.tsx
@@ -5,19 +5,28 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <h1>KEBZ Communications</h1>
-      </div>
-      <nav className="navigation">
-        <ul>
-          {/* <li><Link to="/">Home</Link></li>
-          <li><Link to="/account">Account</Link></li>
-          <li><Link to="/add-plan">Add Plan</Link></li> */}
-          {/* TODO ADD ROUTES */}
+    <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">KEBZ Communications</Link>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/home">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/account/currentuser">Account</Link>
+          </li>
         </ul>
-      </nav>
-    </header>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
   );
 }
 
