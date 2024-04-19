35ab801a382da3703d559dc9ecf5eea105512cca
Karl Enriquez <karlenriquez98@gmail.com>
2024-04-18 21:08:35 -0400

    removed horizontal scrolling from landing page

--- kebzcommfrontend/src/components/devicecardgallery.tsx
+++ kebzcommfrontend/src/components/devicecardgallery.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const DeviceCardGallery: React.FC<{ device: any }> = ({ device }) => {
-
     const navigate = useNavigate();
 
     const handleCardClick = () => {
-        const token = localStorage.getItem('token'); 
-
+        const token = localStorage.getItem('token');
         if (token) {
-            navigate('/home');  // Navigate to HomePage if the user is signed in
+            navigate('/home');
         } else {
-            navigate('/login');  // Navigate to SignIn page if the user is not signed in
+            navigate('/login');
         }
     };
 
     return (
-        <div className="card" style={{ width: '240px', minHeight: '400px', margin: '10px', flex: '0 0 auto', cursor: 'pointer' }} onClick={handleCardClick}>
-            <img src={device.imageUrl} className="card-img-top" alt={device.model} style={{ width: '100%', height: '240px', objectFit: 'contain' }} />
+        <div className="card" style={{
+            width: 'calc(100% - 20px)', // Adjust width to account for padding/margins
+            minHeight: '400px',
+            margin: '10px auto', // Center card horizontally and add vertical spacing
+            cursor: 'pointer'
+        }} onClick={handleCardClick}>
+            <img src={device.imageUrl} className="card-img-top" alt={device.model}
+                style={{ width: '100%', height: '240px', objectFit: 'contain' }} />
             <div className="card-body">
                 <h5 className="card-title">{device.model}</h5>
                 <p className="card-text">{device.manufacturer}</p>
@@ -26,5 +30,6 @@ const DeviceCardGallery: React.FC<{ device: any }> = ({ device }) => {
         </div>
     );
 };
+
 
 export default DeviceCardGallery;
--- kebzcommfrontend/src/components/devicegallery.tsx
+++ kebzcommfrontend/src/components/devicegallery.tsx
@@ -45,7 +45,7 @@ const DeviceGallery: React.FC = () => {
     const [devices, setDevices] = useState<BestDevice[]>(initialDevices);
 
     return (
-        <div className="device-gallery container">
+        <div className="device-gallery container" style={{ maxWidth: '100vw', overflowX: 'hidden', padding: '0 15px' }}>
             <h2>View our best devices.</h2>
             <div className="row">
                 {devices.map(device => (
