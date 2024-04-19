522afd07958e8ab220757768c2f345df6beaef90
Karl Enriquez <karlenriquez98@gmail.com>
2024-04-18 20:31:55 -0400

    Replaced -1 with unlimited

--- kebzcommfrontend/src/components/plangallery.tsx
+++ kebzcommfrontend/src/components/plangallery.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAllPlans } from '../api';
-import { PhonePlan } from '../types'; // Ensure PhonePlan is correctly typed in your types file
+import { PhonePlan } from '../types';
 
 const PlanGallery: React.FC = () => {
     const [plans, setPlans] = useState<PhonePlan[]>([]);
@@ -30,6 +30,8 @@ const PlanGallery: React.FC = () => {
         token ? navigate('/home') : navigate('/register');
     };
 
+    const formatLimit = (limit: number) => limit === -1 ? 'Unlimited' : `${limit}`;
+
     return (
         <div className="container-fluid px-5" style={{ height: '90vh' }}>
             <h2>Discover Our Premier Plans.</h2>
@@ -54,8 +56,8 @@ const PlanGallery: React.FC = () => {
                             <p className="card-text" style={{ fontSize: '1.2rem' }}>{plans[activeTab].planDescription}</p>
                             <ul className="list-unstyled">
                                 <li style={{ fontSize: '1.1rem' }}>Price: ${plans[activeTab].price}</li>
-                                <li style={{ fontSize: '1.1rem' }}>Data Limit: {plans[activeTab].dataLimit} GB</li>
-                                <li style={{ fontSize: '1.1rem' }}>Text Limit: {plans[activeTab].textLimit} texts</li>
+                                <li style={{ fontSize: '1.1rem' }}>Data Limit: {formatLimit(plans[activeTab].dataLimit)}</li>
+                                <li style={{ fontSize: '1.1rem' }}>Text Limit: {formatLimit(plans[activeTab].textLimit)}</li>
                             </ul>
                             <button onClick={handleGetStarted} className="btn btn-primary mt-3">Get Started</button>
                         </div>
